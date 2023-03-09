f07d57cec00a3b89929e317dd27f31e5c87787c2
KhakimDon <92586284+KhakimDon@users.noreply.github.com>
2023-03-09 19:36:09 +0500

    Add files via upload

---
+++ 09.03.23/codewars.js
@@ -0,0 +1,21 @@
+// let min = function(list){
+
+//     return Math.min(...list);
+// }
+
+// let max = function(list){
+
+//     return Math.max(...list);
+// }
+
+// console.log(min([1,2,3,4,5,6]));
+// console.log(max([1,2,3,4,5,6]));
+
+
+
+
+function repeatStr(n, s) {
+  return "";
+}
+
+repeatStr(6,'sardor')
